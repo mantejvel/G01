06261f227fa3107c041230293fb203f77e8ba744
mantejvel <manueltejerovela@gmail.com>
2017-01-12 13:33:30 +0100

    Segundo commit

--- src/prueba/Sum.java
+++ src/prueba/Sum.java
@@ -1,5 +1,8 @@
 package prueba;
 
 public class Sum {
-
+	
+	public static int sum(int a, int d){
+		return a+d;
+	}
 }
--- src/test/TestSum.java
+++ src/test/TestSum.java
@@ -1,5 +1,32 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import prueba.Sum;
+
 public class TestSum {
+	
+	@Test
+	public void test() {
+		assertEquals(6,  sum(2, 4));
+	}
+	
+	@Test
+	public void test1WithZeros() {
+		assertEquals(4,  sum(0, 4));
+		assertEquals(4,  sum(4, 0));
+	}
+	
+	@Test
+	public void test1WithNegs() {
+		assertEquals(10,  sum(12, -2));
+		assertEquals(-7,  sum(-4, -3));
+	}
+	
+	int sum(int a, int b){
+		return Sum.sum(a, b);
+	}
 
 }
